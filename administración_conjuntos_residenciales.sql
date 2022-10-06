ef426446134d7c8c6a361975f1c3d5c785ef0e37
segupe68 <113402029+segupe68@users.noreply.github.com>
2022-10-05 22:11:57 -0500

    se ejecuta tabla registro usuario
    se ejecuta y crea tabla registro usuario

--- administración_conjuntos_residenciales.sql
+++ administración_conjuntos_residenciales.sql
@@ -1,12 +1,17 @@
 CREATE DATABASE administración_conjuntos_residenciales;
+
 CREATE TABLE Registro_Usuario(
 idUsuario int primary key not null,
-IdInmueble int primary key not null,
+IdInmueble int,
+foreign key (IdInmueble) references Inmueble(IdInmueble),
 Nombre varchar (50) not null,
 Apellido varchar (50) not null,
 Numero_Apartamento varchar (50),
 Numero_Telefonico varchar (50)
+
 );
+
+
 
 CREATE TABLE Administrador (
 IdAdministrador int primary key not null,
@@ -25,12 +30,15 @@ TipoInmueble varchar (20) not null,
 piso varchar (20) not null,
 numero int not null
 );
+
+
 CREATE TABLE Obligacion (
-IdInmueble int primary key not null,
 IdObligacion int primary key not null,
+IdInmueble int,
+foreign key (IdInmueble) references Inmueble(IdInmueble),
 Estado int not null,
 pago int primary key not null,
-anio varchar (20) not null,
+año varchar (20) not null,
 mes varchar (20) not null,
 Fvence varchar (20) not null
 );
@@ -39,7 +47,7 @@ CREATE TABLE Factura (
 IdFactura int primary key not null,
 pago int primary key not null,
 mora int primary key not null,
-anio varchar (20) not null,
+año varchar (20) not null,
 mes varchar (20) not null,
 Fvence varchar (20) not null,
 total int not null
