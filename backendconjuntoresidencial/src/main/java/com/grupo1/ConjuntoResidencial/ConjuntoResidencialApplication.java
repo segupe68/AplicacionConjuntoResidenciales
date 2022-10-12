c6ccd08600f0c671ea428a30e8fdc3e30e87f71c
segupe68 <113402029+segupe68@users.noreply.github.com>
2022-10-12 15:37:01 -0500

    creacion Spring Boot
    creación Spring Boot

---
+++ backendconjuntoresidencial/src/main/java/com/grupo1/ConjuntoResidencial/ConjuntoResidencialApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.ConjuntoResidencial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConjuntoResidencialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConjuntoResidencialApplication.class, args);
+	}
+
+}
---
+++ backendconjuntoresidencial/src/test/java/com/grupo1/ConjuntoResidencial/ConjuntoResidencialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.ConjuntoResidencial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConjuntoResidencialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
